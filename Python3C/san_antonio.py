7c3f6c2b2c46a1b0b555d2168b982ba3ca30dc96
Clara Marquois <clara77marquois@gmail.com>
2021-06-10 22:24:11 +0200

    Modification du fichier

--- Python3C/san_antonio.py
+++ Python3C/san_antonio.py
@@ -1,1 +1,16 @@
-print ("Yo ! T'as pas un 06 ?")
+# -*- coding: utf8 -*-
+quotes = [
+    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", 
+    "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
+]
+
+characters = [
+    "alvin et les Chipmunks", 
+    "Babar", 
+    "betty boop", 
+    "calimero", 
+    "casper", 
+    "le chat potté", 
+    "Kirikou"
+]
+
